29e0d81d1400effac6d40c87c853e0b8c25a1bea
oihid <c.ouchene@esi-sba.dz>
2022-10-31 22:25:59 +0100

    onboarding + 1/3auth

--- lib/Screens/Home/Home.dart
+++ lib/Screens/Home/Home.dart
@@ -1,3 +1,4 @@
+import 'package:esilib/size_config.dart';
 import 'package:flutter/material.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,6 +10,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
