ea1543b20964ed072a1d20a3b8c2310c3fc4d20d
oihid <c.ouchene@esi-sba.dz>
2023-02-13 19:49:10 +0100

    modules page+details

---
+++ lib/Screens/Year_resources/Module_details.dart
@@ -0,0 +1,104 @@
+import 'package:esilib/size_config.dart';
+import 'package:flutter/material.dart';
+
+class ModuleDetails extends StatefulWidget {
+  const ModuleDetails({Key? key}) : super(key: key);
+
+  @override
+  State<ModuleDetails> createState() => _ModuleDetailsState();
+}
+
+class _ModuleDetailsState extends State<ModuleDetails> {
+  String module_name='Modulee';
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: getWidth(25)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: getHeight(20),
+              ),
+              Container(
+                constraints: const BoxConstraints(
+                  maxHeight: double.infinity,
+                ),
+                width: getWidth(375),
+                decoration: BoxDecoration(color: Colors.black,
+                borderRadius: BorderRadius.circular(getHeight(20)),
+                ),
+                child: Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: getWidth(13),
+                  vertical: getHeight(15)),
+                  child: Text('ksjdksdksdk,sdk,skd,skd,skd,skd,skd,sjfidgjdngdgddddddddddddddddddddddddddddddddffffdsfskdjgoizeoigjzioejfoizejiofzjeiogjeziogjzeiogjiozegjiozejgiozejgoeikdjviozejgiozejgiozejgiozejgizeklncklzsnfknzknckzcklnsklcnksnckqscklqslkcnqskcnklxnckqsnckqsnckqsncklqsnclknqslc;,qskcnqskfnqsknqlksnflkqsnflkqskdnkzdngkzngdddddddddddd',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: getHeight(18),
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ),
+                ),
+              ),
+              SizedBox(height: getHeight(15),),
+              Text('Resources',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+                fontSize: getHeight(21),
+              ),
+              ),
+              SizedBox(height: getHeight(15),),
+
+              Container(
+                constraints: const BoxConstraints(
+                  maxHeight: double.infinity,
+                ),
+                width: getWidth(375),
+                decoration: BoxDecoration(color: Colors.black,
+                  borderRadius: BorderRadius.circular(getHeight(20)),
+                ),
+                child: Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: getWidth(13),
+                      vertical: getHeight(15)),
+                  child: ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          height: getHeight(15),
+                        );
+                      },
+                      scrollDirection: Axis.vertical,
+                      itemCount: 8,
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int index) =>
+                        InkWell(
+                          child: Row(
+                            children: [
+                              module_name=='Module'
+                              ? Icon(Icons.play_circle_rounded,color: Colors.red,)
+                              :Icon(Icons.book,color:Colors.green,),
+                              SizedBox(width: getWidth(10),),
+                              Text(module_name,
+                              style: TextStyle(
+                                fontSize: getHeight(15),
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600
+                              ),
+                              )
+                            ],
+                          ),
+                        )
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/Year_resources/Resources_screen.dart
+++ lib/Screens/Year_resources/Resources_screen.dart
@@ -1,5 +1,7 @@
+import 'package:esilib/Screens/Year_resources/Module_details.dart';
 import 'package:esilib/size_config.dart';
 import 'package:flutter/material.dart';
+
 class ResourcesPage extends StatefulWidget {
   const ResourcesPage({Key? key}) : super(key: key);
 
@@ -16,43 +18,105 @@ class _ResourcesPageState extends State<ResourcesPage> {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: getHeight(25),),
+            SizedBox(
+              height: getHeight(25),
+            ),
             Padding(
               padding: EdgeInsets.only(left: getWidth(20)),
-              child: Text('The_Selected_Year',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w700,
-                fontSize: getHeight(21),
-              ),
-
+              child: Text(
+                'The_Selected_Year',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: getHeight(21),
+                ),
               ),
             ),
-            SizedBox(height: getHeight(15),),
+            SizedBox(
+              height: getHeight(15),
+            ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: getWidth(20)),
               child: Container(
                 height: getHeight(180),
                 width: getWidth(375),
                 decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                  borderRadius: BorderRadius.circular(getHeight(20))
-                ),
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(getHeight(20))),
               ),
             ),
-            SizedBox(height: getHeight(15),),
-      Padding(
-        padding: EdgeInsets.only(left: getWidth(20)),
-        child: Text('Modules',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w700,
-            fontSize: getHeight(21),
-          ),
-        )
-      ),
-
-
+            SizedBox(
+              height: getHeight(15),
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: getWidth(20)),
+                child: Text(
+                  'Modules',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w700,
+                    fontSize: getHeight(21),
+                  ),
+                )),
+            SizedBox(height: getHeight(13),),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getWidth(20)),
+              child: Container(
+                height: getHeight(460),
+                width: getWidth(375),
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(getHeight(20)),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: getWidth(10),
+                  vertical: getHeight(10)),
+                  child: ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          height: getHeight(15),
+                        );
+                      },
+                      scrollDirection: Axis.vertical,
+                      itemCount: 8,
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int index) =>
+                          SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => const ModuleDetails()),
+                                    );
+                                  },
+                                  child: Container(
+                                    height: getHeight(70),
+                                    width: getWidth(375),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(getHeight(20)),
+                                    ),
+                                    child: Padding(
+                                      padding: EdgeInsets.only(left: getWidth(12),
+                                          top: getHeight(20)),
+                                      child: Text('Module_name',
+                                        style: TextStyle(
+                                            fontSize: getHeight(18),
+                                            fontWeight: FontWeight.w600
+                                        ),),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
