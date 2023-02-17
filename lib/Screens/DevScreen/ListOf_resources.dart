f81a886202e79e9ed7b9c24f9ec2e1450cf9afa2
oihid <c.ouchene@esi-sba.dz>
2023-02-17 19:03:15 +0100

    articles

---
+++ lib/Screens/DevScreen/ListOf_resources.dart
@@ -0,0 +1,56 @@
+List articles = [
+  {
+    'name': 'Flutter Zero to Hero',
+    'image': 'assets/illustrations/Google-flutter-logo.svg',
+    'article':
+        'What is the main purpose of Flutter ? Flutter is an open-source UI software development kit created by Google. It is used to develop cross-platform applications for Android, iOS, Linux, macOS, Windows, Google Fuchsia, and the web from a single codebase.',
+    'resources': [
+      {
+        'id':1,
+        'youtube': 'MJSD Coding',
+        'icon': 'assets/illustrations/icons8-youtube.svg',
+      },
+      {
+        'id':1,
+        'youtube': 'The Flutter Way',
+        'icon': 'assets/illustrations/icons8-youtube.svg',
+      },
+      {
+        'id':1,
+        'youtube': 'HeyFlutter․com',
+        'icon': 'assets/illustrations/icons8-youtube.svg',
+      },
+      {
+        'id':2,
+        'insta': 'bugs_fixes',
+        'icon': 'assets/illustrations/icons8-instagram.svg',
+      },
+      {
+        'id':2,
+        'insta': 'vhemsara',
+        'icon': 'assets/illustrations/icons8-instagram.svg',
+      },
+      {
+        'id':2,
+        'insta': 'codewithflexz',
+        'icon': 'assets/illustrations/icons8-instagram.svg',
+      },
+      {
+        'id':2,
+        'insta': 'flutter.boy',
+        'icon': 'assets/illustrations/icons8-instagram.svg',
+      },
+      {
+        'id':2,
+        'insta': 'flutter.deviser',
+        'icon': 'assets/illustrations/icons8-instagram.svg',
+      },
+      {
+        'id':2,
+        'insta': 'flutter_app_developers',
+        'icon': 'assets/illustrations/icons8-instagram.svg',
+      }
+    ]
+  },
+
+];
--- lib/Screens/DevScreen/article_components.dart
+++ lib/Screens/DevScreen/article_components.dart
@@ -1,3 +1,4 @@
+import 'package:esilib/Screens/DevScreen/ListOf_resources.dart';
 import 'package:esilib/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -11,118 +12,130 @@ class ArticlePage extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.black,
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: getWidth(20)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: getHeight(20),
-              ),
-              Text(
-                'TITLE_OF_Article',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w700,
-                    fontSize: getHeight(
-                      21,
-                    )),
-              ),
-              SizedBox(
-                height: getHeight(15),
-              ),
-              Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(getHeight(20)),
-                  child: SvgPicture.asset(
-                      'assets/illustrations/Python-logo-notext.svg'),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: getHeight(20),
                 ),
-              ),
-              SizedBox(
-                height: getHeight(15),
-              ),
-              Container(
-                constraints: const BoxConstraints(
-                  maxHeight: double.infinity,
+                Text(
+                  articles[0]['name'].toString(),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontSize: getHeight(
+                        21,
+                      )),
                 ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(getHeight(20)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 1,
-                      blurRadius: 9, // changes position of shadow
-                    ),
-                  ],
+                SizedBox(
+                  height: getHeight(15),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: getWidth(10), vertical: getHeight(12)),
-                  child: Text(
-                    'here in this box we going to explain and introduct this thing and give adv and inc its just a test text so i can see how does it works see you next time with diff text ',
-                    style: TextStyle(
-                      fontSize: getHeight(16),
-                      fontWeight: FontWeight.w500,
+                Center(
+                  child: SizedBox(
+                    height: getHeight(100),
+                    width: getWidth(400),
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: SvgPicture.asset(
+                          articles[0]['image'],
+                        width: getHeight(300),
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: getHeight(15),
-              ),
-              Text(
-                'Resources',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w700,
-                    fontSize: getHeight(
-                      21,
-                    )),
-              ),
-              SizedBox(height: getHeight(15),),
-              Container(
-                constraints: const BoxConstraints(
-                  maxHeight: double.infinity,
+                SizedBox(
+                  height: getHeight(15),
                 ),
-                width: getWidth(375),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(getHeight(20)),
+                Container(
+                  constraints: const BoxConstraints(
+                    maxHeight: double.infinity,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(getHeight(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 9, // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: getWidth(10), vertical: getHeight(12)),
+                    child: Text(articles[0]['article'].toString(),
+                      style: TextStyle(
+                        fontSize: getHeight(16),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
                 ),
-                child: Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: getWidth(13),
-                      vertical: getHeight(15)),
-                  child: ListView.separated(
-                      separatorBuilder: (BuildContext context, int index) {
-                        return SizedBox(
-                          height: getHeight(15),
-                        );
-                      },
-                      scrollDirection: Axis.vertical,
-                      itemCount: 8,
-                      shrinkWrap: true,
-                      itemBuilder: (BuildContext context, int index) =>
-                          InkWell(
-                            child: Row(
-                              children: [
-                                const  Icon(Icons.play_circle_rounded,color: Colors.red,),
-                                SizedBox(width: getWidth(10),),
-                                Text('name here',
-                                  style: TextStyle(
-                                      fontSize: getHeight(15),
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w600
+                SizedBox(
+                  height: getHeight(15),
+                ),
+                Text(
+                  'Resources',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontSize: getHeight(
+                        21,
+                      )),
+                ),
+                SizedBox(height: getHeight(15),),
+                Container(
+                  constraints: const BoxConstraints(
+                    maxHeight: double.infinity,
+                  ),
+                  width: getWidth(375),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(getHeight(20)),
+                  ),
+                  child: Padding(
+                    padding:  EdgeInsets.symmetric(horizontal: getWidth(13),
+                        vertical: getHeight(15)),
+                    child: ListView.separated(
+                        separatorBuilder: (BuildContext context, int index) {
+                          return SizedBox(
+                            height: getHeight(15),
+                          );
+                        },
+                        scrollDirection: Axis.vertical,
+                        itemCount: 9,
+                        shrinkWrap: true,
+                        itemBuilder: (BuildContext context, int index) =>
+                            InkWell(
+                              child: Row(
+                                children: [
+                                  SvgPicture.asset(articles[0]['resources'][index]['icon'],
+                                  height: getHeight(30),
                                   ),
-                                )
-                              ],
-                            ),
-                          )
+                                  SizedBox(width: getWidth(10),),
+                                  SelectableText( articles[0]['resources'][index]['id']==1  ?articles[0]['resources'][index]['youtube'].toString() :articles[0]['resources'][index]['insta'].toString(),
+                                    style: TextStyle(
+                                        fontSize: getHeight(15),
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w600
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                    ),
                   ),
                 ),
-              ),
 
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
