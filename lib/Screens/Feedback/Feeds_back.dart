2ba40574895044cb55b470ad6ab2b859279a4bb4
oihid <c.ouchene@esi-sba.dz>
2023-02-17 15:44:40 +0100

    Feedback page

--- lib/Screens/Feedback/Feeds_back.dart
+++ lib/Screens/Feedback/Feeds_back.dart
@@ -8,15 +8,94 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
+  final _controllerSupport=TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return SafeArea(child: Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+      body: Padding(
+        padding:EdgeInsets.symmetric(
+            horizontal: getWidth(20)),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: getHeight(700),
+            child: Column(
 
-        ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: getHeight(20),),
+                Text('Tell Us About ?',
+                style: TextStyle(
+                  color: const Color(0xFF0A27E0),
+                  fontSize: getHeight(25),
+                  fontWeight: FontWeight.w800,
+                ),
+                ),
+                SizedBox(height: getHeight(15),),
+                Container(height: getHeight(230),
+                decoration: BoxDecoration(
+                  color: const Color(0xEDA8A0D5),
+                  borderRadius: BorderRadius.circular(getHeight(20))
+
+                ),
+                  child: Padding(
+                    padding:  EdgeInsets.symmetric(horizontal: getWidth(10)),
+                    child: TextFormField(
+                      controller: _controllerSupport ,
+                      maxLength: 500,
+                      autofocus: false,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      validator: (val) {
+                        if (val!.isEmpty) return "Required";
+                        if (val.length < 10) {
+                          return "Your feedback must be more than 10 words";
+                        }
+                      },
+                      decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Write...'),
+                    ),
+                  ),
+                ),
+                SizedBox(height: getHeight(60),),
+                Padding(
+                  padding: EdgeInsets.only(left:getWidth(132)),
+                  child: InkWell(
+                    child: Container(
+                      height: getHeight(100),
+                      width: getWidth(200),
+                      decoration: BoxDecoration(
+                          color: const Color(0xEDA8A0D5),
+                          borderRadius: BorderRadius.circular(getHeight(20))
+
+                      ),
+                      child: Center(child: Text('Send !',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: getHeight(21)
+                      ),
+                      ),),
+
+                    ),
+
+                  ),
+                ),
+                const Spacer(),
+                Text('here you can type what are the stuff that we did not make it ',
+                style: TextStyle(
+                  fontSize: getHeight(18),
+                  fontWeight: FontWeight.w700
+                ),
+                ),
+                SizedBox(height: getHeight(10),),
+
+              ],
+            ),
+          ),
+        ),
       ),
     ));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:esilib/Screens/SplashScreen/Spalshscreen.dart';
+import 'package:esilib/Screens/Feedback/Feeds_back.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 void main(){
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         highlightColor: Colors.transparent,
         primarySwatch: Colors.blue,
       ),
-      home: MyWidget(),
+      home: const FeedPage(),
     );
   }
 }
